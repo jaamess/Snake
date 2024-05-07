9d535cac0fb84185426e7fc0fe3e3f4f0de7a95a
Thiago <thiagozevedo3@gmail.com>
2024-05-06 21:42:38 -0300

    change gridSize to 25
    now that the game progresses a little bit slower, a larger grid will make it too tediously long

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ const score = document.getElementById('score');
 const highScoreText = document.getElementById('highScore');
 
 // Game variables
-const gridSize = 40;
+const gridSize = 25;
 let snake = [{ x: 10, y: 10 }];
 let food = generateFood();
 let highScore = 0;
